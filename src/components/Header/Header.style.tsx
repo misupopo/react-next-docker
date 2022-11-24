4b842f67b413cb40db1f6fe3a2e48094a49e5c7e
misupopo <kokorononakaniitumo@yahoo.co.jp>
2022-11-24 15:03:26 +0900

    created starter laytou

--- src/components/Header/Header.style.tsx
+++ src/components/Header/Header.style.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
+import { rem } from "polished";
 
 export const HeaderContent = styled.div`
   display: flex;
+  // 10 px を rem に変換
+  margin-top: ${rem(10)};
 `;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import { ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 import { HeaderContent } from "@/components/Header/Header.style";
 
 interface HeaderProps {
+  size?: 'sm' | 'md' | 'lg';
   title: string;
   asChild?: boolean;
   className?: string;
+  children: ReactNode;
 }
 
-export const Header = ({ title, asChild, className }: HeaderProps) => {
+export const Header = ({ size="lg", children, title, asChild, className }: HeaderProps) => {
   const Comp = asChild ? Slot : 'h2';
 
   return (
     <Comp className={clsx(
       'text-gray-100 font-bold font-sans',
-      { 'text-lg': 'sm' },
+      {
+        'text-lg': size === 'sm',
+        'text-xl': size === 'md',
+        'text-2xl': size === 'lg',
+      },
       className
     )}>
       <HeaderContent>
         { title || 'React Next Docker' }
+        <div>{ children }</div>
       </HeaderContent>
     </Comp>
   )
